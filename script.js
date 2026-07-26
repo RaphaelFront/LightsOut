c64eae802f82d69ef634b4afc64307a2ec1f5fb8
RaphaelFront <raphael.front@gmx.net>
2026-07-26 19:57:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+GRID_WIDTH = 5;
+
+const grid = document.querySelector(".grid");
+const boxes = [];
+
+for (let row = 0; row < GRID_WIDTH; row++) {
+    boxes[row] = [];
+    for (let col = 0; col < GRID_WIDTH; col++) {
+        const box = document.createElement("div");
+        box.className = "box";
+
+        boxes[row][col] = box;
+
+        box.dataset.row = row;
+        box.dataset.col = col;
+
+        box.addEventListener("click", () => click(box));
+
+        grid.appendChild(box);
+    }
+}
+
+function click(box) {
+    const x = Number(box.dataset.col);
+    const y = Number(box.dataset.row);
+    
+    toggleBox(x, y);
+    toggleBox(x+1, y);
+    toggleBox(x-1, y);
+    toggleBox(x, y+1);
+    toggleBox(x, y-1);
+
+}
+
+function toggleBox(x, y) {
+    if(x < 0 || y < 0 || x >= GRID_WIDTH || y >= GRID_WIDTH) return;
+    let box = boxes[y][x];
+    if (box.style.backgroundColor === "yellow") {
+        box.style.backgroundColor = "#1d1d1d";
+    } else {
+        box.style.backgroundColor = "yellow";
+    }
+}
